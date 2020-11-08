0a8c2b33623c796e1ff5e9296dbdc8ec6d9f119b
yuan qihang <qihang.yuan@thoughtworks.com>
2020-11-08 16:42:33 +0800

    p3s2

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/Bar.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/Bar.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -7,7 +8,7 @@ public class Bar {
 
     private Foo foo;
 
-    public Bar(Foo foo) {
+    public Bar(@Lazy Foo foo) {
         this.foo = foo;
     }
 
