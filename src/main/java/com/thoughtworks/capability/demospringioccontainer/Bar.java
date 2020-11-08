f1d48855b7d8f2ad5fd527d46a5c105704bab6b9
yuan qihang <qihang.yuan@thoughtworks.com>
2020-11-08 16:37:50 +0800

    p3s1

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/Bar.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/Bar.java
@@ -1,15 +1,14 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Bar {
 
+    @Autowired
     private Foo foo;
 
-    public Bar(Foo foo) {
-        this.foo = foo;
-    }
 
     public void hi() {
         System.out.println("Hi, " + foo.name());
--- src/main/java/com/thoughtworks/capability/demospringioccontainer/Foo.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/Foo.java
@@ -1,15 +1,14 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Foo {
 
+    @Autowired
     private Bar bar;
 
-    public Foo(Bar bar) {
-        this.bar = bar;
-    }
 
     public void hi() {
         System.out.println("Hi, " + bar.name());
